b2212b27de0b8784dc752ca8e02185eaa8f11856
shijinp006 <shijinp9404@gmail.com>
2025-09-15 16:07:03 +0530

    first commit

--- vite-project/src/home/components/landingpage.tsx
+++ vite-project/src/home/components/landingpage.tsx
@@ -6,14 +6,15 @@ import bgImage from "../../assets/vegetables-set-left-black-slate 1.png";
 import { useState } from "react";
 export const LandingPage = () => {
   const [subscribe, setSubscribe] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
   const HandleClick = () => {
-    if (subscribe === true) {
-      setSubscribe(false);
-      alert("Do you want to unsubscribe?");
-    } else {
-      setSubscribe(true);
-      alert("You have successfully subscribed!");
-    }
+    setLoading(true);
+    setTimeout(() => {
+      setSubscribe(!subscribe);
+      setLoading(false);
+
+   
+    }, 1000);
   };
   return (
     <>
@@ -94,10 +95,39 @@ export const LandingPage = () => {
                   <button
                     className={`w-full h-full bg-[#EF294C] text-white text-sm font-medium rounded-full cursor-pointer transition transform hover:scale-95  ${
                       subscribe ? "bg-green-500" : "bg-[#EF294C]"
-                    }`}
+                    }      ${loading ? "opacity-70 cursor-wait" : ""}`}
                     onClick={HandleClick}
+                    disabled={loading}
                   >
-                    {subscribe ? "Subscribed" : "Subscribe"}
+                    {loading ? (
+                      <span className="flex items-center justify-center gap-2">
+                        <svg
+                          className="animate-spin h-4 w-4 text-white"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                          ></path>
+                        </svg>
+                        Please wait...
+                      </span>
+                    ) : subscribe ? (
+                      "Subscribed"
+                    ) : (
+                      "Subscribe"
+                    )}
                   </button>
                 </div>
               </motion.div>
