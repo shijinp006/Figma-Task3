201fb530a04428860953480c2af2702a063e583f
shijinp006 <shijinp9404@gmail.com>
2025-09-15 16:10:34 +0530

    first commit

--- vite-project/src/home/components/landingpage.tsx
+++ vite-project/src/home/components/landingpage.tsx
@@ -121,7 +121,7 @@ export const LandingPage = () => {
                             d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
                           ></path>
                         </svg>
-                        Please wait...
+                
                       </span>
                     ) : subscribe ? (
                       "Subscribed"
